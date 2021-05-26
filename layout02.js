69fa5f6dab7a622a07adad6bd14eaceadadba6da
a.tkachenko <profuel@gmail.com>
2021-05-26 13:50:53 +0200

    added 2 more layouts with cross-links added titles

---
+++ layout02.js
@@ -0,0 +1,13 @@
+/*----------- Game State Data ----------*/
+
+const board = [
+    null, null, null, null, null, null, null,
+    null, null, null, 46, null, null, null,
+    null, null, null, 45, null, null, null,
+    null, 24, 34, 44, 54, 64, null,
+    null, null, null, 43, null, null, null,
+    null, null, null, 42, null, null, null,
+    null, null, null, null, null, null, null
+];
+const boardSize = 7;
+let redScore = 32;
